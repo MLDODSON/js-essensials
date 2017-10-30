c9cd0575602538ddfddae44c1ada3955f7330216
MLDODSON <mldodson@fullsail.edu>
2017-10-30 15:27:52 -0500

    basics

---
+++ basicvars.js
@@ -0,0 +1,19 @@
+// basic string variable
+
+var name = "Chuck";
+console.log(name);
+
+// A var can be assigned a new value
+
+name = "Dr. Strangelove";
+// Will take the place of Chuck
+
+// A value can be left empty until it is assigned something
+var empty;
+empty = "I am full.";
+
+// jS is a loose language. You can change data types in your var.
+empty = "10 + 6";
+
+// This is a constant which always remains the same always. It must be assigned a value. A new value can never be reassigned once one is chosen.
+const lastName = "Megatron";
