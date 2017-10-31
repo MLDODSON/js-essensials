58b30ab284393cfee5d1f3d1d33d854c15b3acc7
MLDODSON <mldodson@fullsail.edu>
2017-10-30 21:22:21 -0500

    functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+// Functions.
+
+function butter() {
+	alert("Who stole my chicken and ate the last piece of cake?");
+}
+
+butter();
+
+// The line of code inside your function is called a decleration.
