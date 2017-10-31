650a7569a0e2cc4bcee644ff059fffec8aa9112f
MLDODSON <mldodson@fullsail.edu>
2017-10-31 10:16:34 -0500

    parameters

--- functions.js
+++ functions.js
@@ -7,3 +7,30 @@ function butter() {
 butter();
 
 // The line of code inside your function is called a decleration.
+function funkadelic() {
+	alert("Not just knee deep.");
+}
+
+funkadelic();
+
+// Using a function with a parameter
+function wife(w) {
+	alert("I just love " + w);
+}
+
+wife("Glenny.");
+
+function squad(c) {
+	alert("The baseball Cardinals are my favorite " + c);
+}
+
+squad("team.");
+
+// You can also pass multiple values through the function.
+function food(x) {
+	alert("I love eating " + x);
+}
+
+food("steak.");
+food("tuna.");
+food("chocolate.");
