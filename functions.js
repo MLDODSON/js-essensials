2c0891a468642a01a26eca69e274f0bc01d8ef19
MLDODSON <mldodson@fullsail.edu>
2017-10-31 10:35:08 -0500

    functions with multi parameters

--- functions.js
+++ functions.js
@@ -34,3 +34,20 @@ function food(x) {
 food("steak.");
 food("tuna.");
 food("chocolate.");
+
+// Using multi parameters
+function places(a, b) {
+	console.log(a + " and " + b + " are nice towns to visit.");
+}
+
+places("Puerto Plata", "Santiago");
+
+function comics(c, d, e) {
+	console.log("My favorite super heroes are " + c + ", " + d + " and " + e);
+}
+
+comics("Spider-Man", "Batman", "The Hulk.");
+comics("Thor", "The Flash", "Wolverine.");
+comics("Black Panther", "Martian Manhunter", "The Silver Surfer.");
+
+
