386bfe775f761207819674f71df262a4f7d43185
MLDODSON <mldodson@fullsail.edu>
2017-11-02 20:22:39 -0500

    functions quiz

---
+++ functionsquiz.js
@@ -0,0 +1,172 @@
+// Functions Quiz, declerations.
+
+function cheese() {
+	alert("Friday is payday!");
+}
+
+cheese();
+
+
+function trip() {
+	alert("I love to visit Puerto Plata in the Dominican Republic.");
+}
+
+trip();
+
+
+function exercise() {
+	alert("Time to get your butt back in shape!");
+}
+
+exercise();
+
+
+function education() {
+	alert("Coding is fun!");
+}
+
+education();
+
+
+function villian() {
+	alert("Do not mess with Thanos of Titan!");
+}
+
+villian();
+
+
+function greatest() {
+	alert("Jerry Rice is the best to ever play in the NFL.")
+}
+
+greatest();
+
+
+function movie() {
+	alert("Thor: Ragnorak opens tonight!");
+}
+
+movie();
+
+
+function stop() {
+	alert("Put my candy down now!");
+}
+
+stop();
+
+
+function dessert() {
+	alert("Cherry cheesecake for everyone!");
+}
+
+dessert();
+
+
+function groceries() {
+	alert("Do not forget my Chips Ahoys this time!");
+}
+
+groceries();
+
+
+
+//Functions Quiz, parameters.
+function person(a) {
+	alert("I love " + a);
+}
+
+person("Mom!");
+
+
+function move(b) {
+	alert("Someday I want to live in " + b);
+}
+
+move("Orlando, Florida.");
+
+
+function age(c) {
+	alert("Mrs. Wilson just turned " + c);
+}
+
+age("one hundred!");
+
+
+function conflict(d) {
+	alert("They get along like " + d);
+}
+
+conflict("oil and water.");
+
+
+function music(e) {
+	alert("My favorite music group is " + e);
+}
+
+music("Parliament/Funkadelic.");
+
+
+function relax(f)  {
+	alert("I love to kick back and listen to " + f);
+}
+		  
+relax("some Jazz!");
+
+
+function growth(g) {
+	alert("We all must fall before we " + g);
+}
+
+growth("walk.");
+
+
+function boom(h) {
+	alert("The bigger they are, the harder they " + h);
+}
+
+boom("fall!");
+
+
+function binge(i) {
+	alert("I could watch " + i + " all day!");
+}
+
+binge("Netflix");
+
+
+function tasty(j) {
+	alert(j + " is a delicious delight!");
+}
+
+tasty("Devil's food cake");
+
+
+// function passing multiple values through.
+function artist(x) {
+	alert("I love listening to " + x);
+}
+
+artist("Prince.");
+artist("Michael Jackson.");
+artist("Quincy Jones.");
+
+
+function player(l) {
+	alert(l + " is my favorite NBA Player of all time.")
+}
+
+player("Dr. J.");
+player("Moses Malone");
+player("Chocolate Thunder");
+
+function hobby(m) {
+	alert("I really enjoy " + m + " in my spare time.");
+}
+
+hobby("reading Stephen King novels");
+hobby("watching classic movies");
+hobby("listening to old vinyl records");
+hobby("reading comic books");
+hobby("visiting my aunt for talks");
+
