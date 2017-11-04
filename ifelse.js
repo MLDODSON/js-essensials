1d7b9853c0ccae7773cea2deb85cdf03634dfe3a
MLDODSON <mldodson@fullsail.edu>
2017-11-03 20:45:19 -0500

    if else statement lessons

---
+++ ifelse.js
@@ -0,0 +1,144 @@
+// If Statements
+
+var rice = 17;
+var pudding = 38;
+
+if(rice<pudding) {
+	console.log("I want some of that please.");
+}
+
+if(38===38) {
+	console.log("It's a beautiful day in the neighborhood.");
+}
+
+var catfish = 50;
+var tunafish = 45;
+
+if(catfish>=tunafish) {
+	console.log("Let's do lunch.");
+}
+
+if(16!==39) {
+	console.log("I want a Yeaster Egg, I want a Yeaster Egg!");
+}
+
+var darkKnight = 75;
+var inspectorGadget = 80;
+
+if(darkKnight<=inspectorGadget) {
+	console.log("Go gadget go!");
+}
+
+// If Else Statements
+
+if(13===13) {
+	console.log("Pour me another glass of wine.");
+} else {
+	console.log("A budlight please.");
+}
+
+var up = 56;
+var down =30;
+
+if(up<down) {
+	console.log("Free drinks on the house!");
+} else {
+	console.log("Are you crazy!");
+}
+
+if(9!==17) {
+	console.log("We are going to Wendy's.");
+} else {
+	console.log("We are going to Burger King.");
+}
+
+var cat = 25;
+var dog = 22;
+
+if(cat>=dog) {
+	console.log("I will have a rootbeer soda.");
+} else {
+	console.log("I will have a grape soda.");
+}
+
+var light = 12;
+var dark = 11;
+
+if(light>dark) {
+	console.log("Miami here we come!");
+} else {
+	console.log("Oh no! I don't want to go there!");
+}
+
+var joy = 9;
+var pain = 10;
+
+if(joy<=pain) {
+	console.log("This is the worst day ever!");
+} else {
+	console.log("I feel good!");
+}
+
+var bitter = 4;
+var sweet = 5;
+
+if(bitter<=sweet) {
+	console.log("Turn on HBO.");
+} else {
+	console.log("Let's watch some Netflix.");
+}
+
+if(13>=15) {
+	console.log("We are going fishing.");
+} else {
+	console.log("We are going swimming.");
+}
+
+var red =  16;
+var blue =  30;
+
+if(red===16) {
+	console.log("I will have a piece of cherry pie.");
+} else {
+	console.log(" will have a piece of pecan pie.");
+}
+
+var ego = 30;
+var maniac = 35;
+
+if(maniac===35) {
+	console.log("I will rule the world with my plans!");
+} else {
+	console.log("I think I'll go to bed now.");
+}
+
+// Functions with a single and multi parameters
+function yummy(a) {
+	console.log("Put some " + a + " on my ice cream.");
+}
+
+yummy("chocolate syrup");
+
+function music(a,b) {
+	console.log("Make my " + a + " the " + b + " I want to get funked up!");
+}
+
+music("funk", "P-funk.");
+
+function creepy(a) {
+	console.log("Stop trying to " + a + " people.")
+}
+
+creepy("scare");
+
+function food(a) {
+	console.log("I want " + a + " fries with that shake!");
+}
+
+food("fries");
+
+function happy(a,b) {
+	console.log("I will have " + a + " and watch a " + b);
+}
+
+happy("pizza", "movie.");
