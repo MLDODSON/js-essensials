bc89f566de9c74e7ae9e0cef3bef0d14432f782f
MLDODSON <mldodson@fullsail.edu>
2017-11-14 00:27:10 -0600

    practice

--- objects.js
+++ objects.js
@@ -4,3 +4,35 @@
 var leon = "Leon is the man!";
 console.log(leon.length);
 
+
+function person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+
+//Constructor function
+//Creating a new instance of an object 
+var rod = new person("Rod Munch", 49);
+var glenny = new person("Glenny Perez", 30);
+
+console.log(rod.age);
+console.log(glenny.name, glenny.age);
+
+
+function lady(name, age, eyes, hair) {
+	this.name = name;
+	this.hair = hair;
+	this.eyes = eyes;
+	this.age = age;
+}
+
+var paola = new lady("Paola Perez", "blondie", "brown", 21);
+
+console.log(paola.name, paola.hair, paola.eyes, paola.age);
+
+
+//Object Initializers
+mike = {name:"Michael Hunter", age:39};
+kim = {name:"Kim Taylor", age:25};
+
+console.log(mike.name + " is " + mike.age + " and " + kim.name + " is " + kim.age + ".");
---
+++ objectstwo.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+var cats = ["Ebony", "Boomer", "Bootsy", "Bobo"];
+
+for (var a = 0; a < cats.length; a++) {
+	console.log(cats[a]);
+}
+
+var people = ["Dan", "Tony", "Barbara", "Larry", "Cid"];
+
+for (var y = 0; y < 5; y++) {
+	console.log(people[y]);
+}
