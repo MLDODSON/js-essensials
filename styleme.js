d76b4f08016f629b28e7886092036a7128455a40
MLDODSON <mldodson@fullsail.edu>
2017-12-02 22:20:48 -0600

    styling with js

---
+++ styleme.js
@@ -0,0 +1,44 @@
+// Styling an element.
+
+var el = document.getElementById("style");
+
+/*el.style.background = "purple";
+el.style.color = "white";
+el.style.width = "200px";
+el.style.margin = "10px";
+el.style.textAlign = "center";*/
+
+el.style.cssText = "background:purple; color:white; width:200px; margin:20px; text-align:center; border:4px solid green; padding:10px";
+
+el.style.cssText += " height:100px; ";
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
